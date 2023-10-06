5cad53c4a9a88d3bcf8524e6e533ab4c025d746c
Nate Moore <nate@astro.build>
2023-10-05 23:00:56 -0500

    remove site and base from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://projects.natemoo.re/',
-    base: 'pandoras-box/'
-});
+export default defineConfig({});
